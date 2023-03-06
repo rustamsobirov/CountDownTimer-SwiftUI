e8247b52bf889bc310fd72cb8bd600d82f796589
admin <carwolt123@gmail.com>
2023-03-06 15:27:50 +0500

    Initial Commit

---
+++ CountDownTimer SwiftUI/CountDownTimer_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountDownTimer_SwiftUIApp.swift
+//  CountDownTimer SwiftUI
+//
+//  Created by iMac G1 on 06/03/23.
+//
+
+import SwiftUI
+
+@main
+struct CountDownTimer_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
